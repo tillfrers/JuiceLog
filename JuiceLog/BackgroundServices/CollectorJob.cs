3bba5784a3302171c49e34101101c6f1476d933e
tillf <tillfrers@posteo.de>
2025-09-18 20:25:57 +0200

    renamed from Gas-Electricity-Logger.AssemblyInfo to JuiceLog

---
+++ JuiceLog/BackgroundServices/CollectorJob.cs
@@ -0,0 +1,15 @@
+﻿using Quartz;
+
+namespace JuiceLog.BackgroundServices;
+
+public class CollectorJob : IJob
+{
+    public static readonly JobKey JobKey = new("Collector", "BackgroundJob");
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+
+        Console.WriteLine("Test");
+        
+    }
+}
---
+++ JuiceLog/ConfigureServices.cs
@@ -0,0 +1,26 @@
+﻿using JuiceLog.BackgroundServices;
+
+namespace JuiceLog;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        /*services.AddDbContext<A1DbContext>((serviceProvider, options) =>
+        {
+            var secretConnectionStringService = serviceProvider.GetRequiredService<ISecretConnectionStringService>();
+
+            options.UseNpgsql(
+                secretConnectionStringService.GetConnectionString(),
+                o => o.MigrationsHistoryTable(
+                    tableName: HistoryRepository.DefaultTableName,
+                    schema: A1DbContext.Schema));
+        });
+
+        services.AddScoped<IMeldedateiRepository, MeldedateiRepository>();
+*/
+        services.AddScoped<CollectorJob>();
+
+        return services;
+    }
+}
